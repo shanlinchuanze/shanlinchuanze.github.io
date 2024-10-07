2e44081485d775865c107c3f3cc3a21be6cd1f07
shanlinchuanze <2807804431@qq.com>
2024-10-07 22:19:42 +0800

    Site updated: 2024-10-07 22:19:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/29/我个人对于栈空间的理解/"];function toRandomPost(){
+var posts=["2024/09/29/我个人对于栈空间的理解/","2024/10/07/Newshar-Real-Login/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
