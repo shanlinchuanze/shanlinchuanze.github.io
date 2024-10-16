fe87462ec92820455b92ea8bc2001528db497d91
shanlinchuanze <2807804431@qq.com>
2024-10-16 12:46:43 +0800

    Site updated: 2024-10-16 12:46:42

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/07/Newshar-Real-Login/","2024/09/29/我个人对于栈空间的理解/","2024/10/15/10-15CTF/"];function toRandomPost(){
+var posts=["2024/10/07/Newshar-Real-Login/","2024/09/29/我个人对于栈空间的理解/","2024/10/15/10-15CTF/","2024/10/16/10-16CTF/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
